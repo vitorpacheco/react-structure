4c503d5d61947c81238c157c7cbe9473deb7e3c7
Vitor Pacheco Costa <vitor.costa@solutis.com.br>
2019-08-27 16:22:46 -0300

    Commit formatação de código

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -8,14 +8,14 @@ import { Wrapper } from './styles';
 const DefaultLayout = ({ children }) => {
   return (
     <Wrapper>
-      <Header/>
+      <Header />
       {children}
     </Wrapper>
   );
 };
 
 DefaultLayout.propTypes = {
-  children: PropTypes.element.isRequired
+  children: PropTypes.element.isRequired,
 };
 
 export default DefaultLayout;
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,22 +1,22 @@
 export const signInRequest = (email, password) => ({
   type: '@auth/SIGN_IN_REQUEST',
-  payload: { email, password }
+  payload: { email, password },
 });
 
 export const signInSuccess = (token, user) => ({
   type: '@auth/SIGN_IN_SUCCESS',
-  payload: { token, user }
+  payload: { token, user },
 });
 
 export const signUpRequest = (name, email, password) => ({
   type: '@auth/SIGN_UP_REQUEST',
-  payload: { name, email, password }
+  payload: { name, email, password },
 });
 
 export const signFailure = () => ({
-  type: '@auth/SIGN_FAILURE'
+  type: '@auth/SIGN_FAILURE',
 });
 
 export const signOut = () => ({
-  type: '@auth/SIGN_OUT'
+  type: '@auth/SIGN_OUT',
 });
